622b3a91ed9be0366519448d276ebfab38ca7934
chadspector <chad.spector@mail.mcgill.ca>
2022-11-24 15:35:37 -0500

    initialized java project

---
+++ group30_project/src/main/java/group30/App.java
@@ -0,0 +1,13 @@
+package group30;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
