79be8ff426cca90e0d93a413243334021df3cfcf
chadspector <chad.spector@mail.mcgill.ca>
2022-12-04 17:46:04 -0500

    added maven + jenkins config

---
+++ group30_project/src/test/java/group30/JunitTestSuite.java
@@ -0,0 +1,17 @@
+package group30;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+                AdditionTest.class,
+                SubtractionTest.class,
+                MultiplicationTest.class,
+                DivisionTest.class,
+                ExponentTest.class,
+                SquareRootTest.class,
+                LogarithmTest.class,
+})
+public class JunitTestSuite {
+}
